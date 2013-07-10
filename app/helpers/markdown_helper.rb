275767465ebd6394a176f7b1897733b371b69d27
Vrixyz <tom.kahl@hotmail.fr>
2013-07-10 16:18:27 +0200

    fixed an security hole which could allow users to post js code in posts

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -4,8 +4,8 @@ module MarkdownHelper
 
   def markdown_to_html(md)
     renderer = Redcarpet::Render::HTML.new
-    extensions = {}
+    extensions = {hard_wrap: true}
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
-    return redcarpet.render md.html_safe
+    return redcarpet.render(sanitize(md)).html_safe
   end
 end
