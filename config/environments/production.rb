6afcb40c833f40f8cfd8535fa5647698257edd8e
Vrixyz <tom.kahl@hotmail.fr>
2013-07-12 17:27:24 +0200

    url fixes to precompile js

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Ptts::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( navigation.js )
+  config.assets.precompile += %w( games/navigation/navigation.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
